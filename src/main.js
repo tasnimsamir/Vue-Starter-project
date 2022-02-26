979638ea02ea11445504d3c284e546d711b7efce
Tasnim Samir <tasnimsamir71@gmail.com>
2022-02-26 17:57:16 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import i18n from './i18n'
+import store from './store'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  i18n,
+  store,
+  router,
+  render: h => h(App)
 }).$mount('#app')
