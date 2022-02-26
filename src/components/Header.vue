fa3acf5b11f043045e12a6a546b3c74ff6dbbe79
Tasnim Samir <tasnimsamir71@gmail.com>
2022-02-26 18:42:15 +0200

    file structures

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Header-comp',
+  props: {},
+  methods: {},
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Search-comp',
+  props: {},
+  methods: {},
+}
+</script>
+
+<style scoped>
+
+</style>
